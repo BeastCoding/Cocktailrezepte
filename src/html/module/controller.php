f33e00853443eda3ba63c3c5806b7786fcb67a40
BeastCoding <bastian.viersen@googlemail.com>
2018-12-01 15:54:30 +0100

    commit to Master

--- src/html/module/controller.php
+++ src/html/module/controller.php
@@ -1,1 +1,55 @@
 # controller for SQL/Html control
+<?php
+include ("interface_SQL.php");
+
+
+//Verbindungsaufbau
+$con = mysqli_connect("localhost", "Nutzername", "Passwort", "Datenbank");
+//Test, ob Verbindung aufgebaut wurde
+if (!$con) {
+echo "Verbindung fehlgeschlagen!";
+exit;
+}
+else {
+echo "Verbindung erfolgreich aufgebaut";
+}
+
+// Join of Cocktail and Zutat
+function get_Cocktail($name){
+	$query = "
+	SELECT Cocktail.Name, 
+		Cocktail.Zubereitung, 
+		Cocktail.Alkohol,
+		Cocktail_Zutat.Menge,
+		Zutat.Name,
+		Zutat.Einheit,
+		Zutat.Kalorien
+	FROM(
+		Cocktail_Zutat
+	INNER JOIN 
+		Zutat
+	ON
+		Cocktail_Zutat.ZutatID = Zutat.ID)
+	INNER JOIN 
+		Cocktail
+	ON
+		Cocktail_Zutat.CocktailID = Cocktail.ID
+	WHERE
+		Cocktail.Name ="+ $name;
+	$res = mysqli_query($con, $query);
+	return mysqli_fetch_assoc($res);
+}
+
+
+// fetches Zutat of a specific name
+function get_Zutat($name){
+	$select = "Name, Einheit, Kalorien";
+	$where = "Name ="+$ name;
+	return select_SQL($select,"Zutat", $where);
+	
+}
+
+
+
+mysqli_close($con);
+?>
--- src/html/module/interfaceSQL.php
+++ src/html/module/interfaceSQL.php
@@ -1,8 +1,23 @@
 <?php
 
-function search_Sql(,,){
+//function for select statement
+function select_Sql($select,$from,$where){
+	$query = "SELECT"+$select+"FROM"+$from+"WHERE"+$where;
+	$res = mysqli_query($con, $query);
+	return mysqli_fetch_assoc($res);
+}
+
+//funktion for join statement
+function join_Sql($select,$from,$join,$on){
+	$query = "SELECT"+$select+"FROM"+$from+$join+"ON"+$on;
+	$res = mysqli_query($con, $query);
+	return mysqli_fetch_assoc($res);
 	
 }
+
+
+
+
 
 function write_Sql(,,){
 	
@@ -11,4 +26,5 @@ function write_Sql(,,){
 function read_Sql(,,){
 	
 }
+
 ?>
