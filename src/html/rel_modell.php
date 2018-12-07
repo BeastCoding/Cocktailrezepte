bc4e4b5b550c2ba86ef900ed8501a47d01c26aec
BeastCoding <bastian.viersen@googlemail.com>
2018-12-07 17:44:53 +0100

    fix

--- src/html/rel_modell.php
+++ src/html/rel_modell.php
@@ -1,18 +1,17 @@
 ﻿<?php
 include('module/header.php')
 ?>
-	<body>  
+	<body>
 		<?php
 		include('module/menu.php')
 		?>
 		<div class="space1"></div>
 		<section id="hauptteil">
-				
-				<img class="rel_modell" src="../../img/Rel Modell final.jpg" alt="Relation_Modell">			
-							
-			
+
+				<img class="rel_modell" src="../img/Rel Modell final.jpg" alt="Relation_Modell">			
+
+
 		</section>
 <?php
 include('module/footer.php')
 ?>
-
