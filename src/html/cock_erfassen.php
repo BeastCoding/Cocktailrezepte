982a68371a84afbdbda418c9a2d739f32457da1b
Bkalembasi <b.kalembasi@web.de>
2018-12-16 17:55:25 +0100

    edit

--- src/html/cock_erfassen.php
+++ src/html/cock_erfassen.php
@@ -1,20 +1,6 @@
 ﻿<?php
-include('module/header.php')
-?>
-<?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$db = "pswd18_5";
-try {
-$conn = new PDO("mysql:host=$servername;dbname=$db;", $username, $password);
-// set the PDO error mode to exception
-$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch(PDOException $e)
-{
-echo "Connection failed: " . $e->getMessage();
-}
+include('module/header.php');
+include('module/pdo_zugang.php');
 ?>
 <script src="https://code.jquery.com/jquery-1.10.2.js"></script>
 
@@ -24,52 +10,52 @@ echo "Connection failed: " . $e->getMessage();
 		?>
 	<div class="space1"></div>
 	<section id="hauptteil">
-		<form method="get">
+		<!--<form method="get">-->
 
-			<!---------------------- Cocktailname ---------------------->
-			<fieldset>
-				<legend>Einen neuen Cocktail kreieren</legend>
-				Name des Cocktails:
-				<input type="text" name="cocktailname">
-			</fieldset>
-			<!---------------------- Cocktailname ---------------------->
+		<!---------------------- Cocktailname ---------------------->
+		<fieldset>
+			<legend>Einen neuen Cocktail kreieren</legend>
+			Name des Cocktails:
+			<input type="text" name="cocktailname">
+		</fieldset>
+		<!---------------------- Cocktailname ---------------------->
 
-			<!---------------------- Zubereitung ---------------------->
-			<fieldset>
-				<legend>Zubereitung</legend>
-				<textarea></textarea>
-			</fieldset>
-			<!---------------------- Zubereitung ---------------------->
+		<!---------------------- Zubereitung ---------------------->
+		<fieldset>
+			<legend>Zubereitung</legend>
+			<textarea></textarea>
+		</fieldset>
+		<!---------------------- Zubereitung ---------------------->
 
-			<!---------------------- Zutaten ---------------------->
-			<fieldset class="fieldset-auto-width">
-				<legend>Zutaten</legend>
-				<button id="btnAdd">+</button>
-				<button id="btnDel">-</button>
-				<div id="input1" class="clonedInput">
-					<label>Zutat:</label>
-					<input class="feldgroesse" type="text" name="zutat1">
-					<label>Einheit:</label>
-					<select class="feldgroesse" name="Einheit" size="1">
-						<option class="feldgroesse" alue="e11">Gramm</option>
-						<option class="feldgroesse" value="e12">cl</option>
-						<option class="feldgroesse" value="e13">Stück</option>
-						<option class="feldgroesse" value="e14">Scheibe</option>
-						<option class="feldgroesse" value="e15">ml</option>
-						<option class="feldgroesse" value="e16">Prise</option>
-						<option class="feldgroesse" value="e17">EL</option>
-					</select>
-					<label>Menge:</label>
-					<input class="feldgroesse" type="text" name="menge1">
-				</div>
-				<br>
-			</fieldset>
-			<!---------------------- Zutaten ---------------------->
+		<!---------------------- Zutaten ---------------------->
+		<fieldset class="fieldset-auto-width">
+			<legend>Zutaten</legend>
+			<button id="btnAdd">+</button>
+			<button id="btnDel">-</button>
+			<div id="input1" class="clonedInput">
+				<label>Zutat:</label>
+				<input class="feldgroesse" type="text" name="zutat1">
+				<label>Einheit:</label>
+				<select class="feldgroesse" name="Einheit" size="1">
+					<option class="feldgroesse" alue="e11">Gramm</option>
+					<option class="feldgroesse" value="e12">cl</option>
+					<option class="feldgroesse" value="e13">Stück</option>
+					<option class="feldgroesse" value="e14">Scheibe</option>
+					<option class="feldgroesse" value="e15">ml</option>
+					<option class="feldgroesse" value="e16">Prise</option>
+					<option class="feldgroesse" value="e17">EL</option>
+				</select>
+				<label>Menge:</label>
+				<input class="feldgroesse" type="text" name="menge1">
+			</div>
+			<br>
+		</fieldset>
+		<!---------------------- Zutaten ---------------------->
 
-			<!---------------------- Cocktailgeschmack ---------------------->
-			<fieldset class="fieldset-auto-width">
-				<legend>Cocktailgeschmack</legend>
-				<?php
+		<!---------------------- Cocktailgeschmack ---------------------->
+		<fieldset class="fieldset-auto-width">
+			<legend>Cocktailgeschmack</legend>
+			<?php
 						$sql  = 'SELECT *  FROM `geschmack`';
 						foreach ($conn->query($sql) as $row) {
 							switch				 ($row['ID'])
@@ -114,14 +100,14 @@ echo "Connection failed: " . $e->getMessage();
 						   echo "<input type='checkbox' name='art' value= " . $wert . ">" . $row['Geschmacksrichtung'] . "<br>";
 						}
 					?>
-				<br>
-			</fieldset>
-			<!---------------------- Cocktailgeschmack ---------------------->
+			<br>
+		</fieldset>
+		<!---------------------- Cocktailgeschmack ---------------------->
 
-			<!---------------------- Cocktailglas ---------------------->
-			<fieldset class="fieldset-auto-width">
-				<legend>Cocktailglas</legend>
-				<?php
+		<!---------------------- Cocktailglas ---------------------->
+		<fieldset class="fieldset-auto-width">
+			<legend>Cocktailglas</legend>
+			<?php
 						$sql  = 'SELECT *  FROM `glas`';
 						foreach ($conn->query($sql) as $row) {
 							switch				 ($row['ID'])
@@ -166,14 +152,14 @@ echo "Connection failed: " . $e->getMessage();
 						   echo "<input type='checkbox' name='art' value= " . $wert . ">" . $row['Glastyp'] . "<br>";
 						}
 					?>
-				<br>
-			</fieldset>
-			<!---------------------- Cocktailglas ---------------------->
+			<br>
+		</fieldset>
+		<!---------------------- Cocktailglas ---------------------->
 
-			<!---------------------- Cocktailart ---------------------->
-			<fieldset class="fieldset-auto-width">
-				<legend>Cocktailart</legend>
-				<?php
+		<!---------------------- Cocktailart ---------------------->
+		<fieldset class="fieldset-auto-width">
+			<legend>Cocktailart</legend>
+			<?php
 						$sql  = 'SELECT *  FROM `kategorie`';
 						foreach ($conn->query($sql) as $row) {
 							switch				 ($row['ID'])
@@ -200,14 +186,15 @@ echo "Connection failed: " . $e->getMessage();
 						   echo "<input type='checkbox' name='art' value= " . $wert . ">" . $row['Cocktailart'] . "<br>";
 						}
 					?>
-				<br>
-			</fieldset>
-			c
+			<br>
+		</fieldset>
+		<!---------------------- Cocktailart ---------------------->
 
-			<!---------------------- Dekoration ---------------------->
-			<fieldset class="fieldset-auto-width">
-				<legend>Dekoration</legend>
-				<?php
+
+		<!---------------------- Dekoration ---------------------->
+		<fieldset class="fieldset-auto-width">
+			<legend>Dekoration</legend>
+			<?php
 						$sql  = 'SELECT *  FROM `einzeldeko`';
 						foreach ($conn->query($sql) as $row) {
 							switch				 ($row['ID'])
@@ -234,14 +221,14 @@ echo "Connection failed: " . $e->getMessage();
 						   echo "<input type='checkbox' name='art' value= " . $wert . ">" . $row['Dekosorte'] . "<br>";
 						}
 					?>
-				<br>
-			</fieldset>
-			<!---------------------- Dekoration ---------------------->
+			<br>
+		</fieldset>
+		<!---------------------- Dekoration ---------------------->
 
-			<!---------------------- Trinkanlass ---------------------->
-			<fieldset class="fieldset-auto-width">
-				<legend>Trinkanlass</legend>
-				<?php
+		<!---------------------- Trinkanlass ---------------------->
+		<fieldset class="fieldset-auto-width">
+			<legend>Trinkanlass</legend>
+			<?php
 						$sql  = 'SELECT *  FROM `anlass`';
 						foreach ($conn->query($sql) as $row) {
 							switch				 ($row['ID'])
@@ -265,47 +252,54 @@ echo "Connection failed: " . $e->getMessage();
 						   echo "<input type='checkbox' name='art' value= " . $wert . ">" . $row['Trinkanlass'] . "<br>";
 						}
 					?>
-				<br>
-			</fieldset>
-			<!---------------------- Trinkanlass ---------------------->
+			<br>
+		</fieldset>
+		<!---------------------- Trinkanlass ---------------------->
 
 
-			<!---------------------- Alkoholgehalt ---------------------->
-			<fieldset class="fieldset-auto-width">
-				<legend>Alkoholgehalt</legend>
-				<input type="checkbox" name="alkohol" value="frei">alkoholfrei<br>
-				<input type="checkbox" name="alkohol" value="maessig">mäßig<br>
-				<input type="checkbox" name="alkohol" value="normal">normal<br>
-				<input type="checkbox" name="alkohol" value="stark">stark<br>
-				<br>
-			</fieldset>
-			<!---------------------- Alkoholgehalt ---------------------->
+		<!---------------------- Alkoholgehalt ---------------------->
+		<fieldset class="fieldset-auto-width">
+			<legend>Alkoholgehalt</legend>
+			<input type="checkbox" name="alkohol" value="frei">alkoholfrei<br>
+			<input type="checkbox" name="alkohol" value="maessig">mäßig<br>
+			<input type="checkbox" name="alkohol" value="normal">normal<br>
+			<input type="checkbox" name="alkohol" value="stark">stark<br>
+			<br>
+		</fieldset>
+		<!---------------------- Alkoholgehalt ---------------------->
 
-			<!--<input type="Submit" name="speichern" value="speichern">-->
-		</form>
+		<input type="Submit" name="speichern" value="speichern">
+		<!--</form> -->
 	</section>
 
+	<!---------------------- Zutat Addieren Script ---------------------->
 	<script>
-		$("#btnAdd").click(function() {
+		$('#btnAdd').click(function() {
 			var num = $('.clonedInput').length;
 			var newNum = new Number(num + 1);
 			var newElem = $('#input' + num).clone().attr('id', 'input' + newNum);
 			newElem.children(':first').attr('id', 'name' + newNum).attr('name', 'name' + newNum);
 			$('#input' + num).after(newElem);
-			$('#btnDel').attr('disabled', '');
-			if (newNum == 5) $('#btnAdd').attr('disabled', 'disabled');
+			$('#btnDel').attr('disabled', false);
+			if (newNum == 5)
+				$('#btnAdd').attr('disabled', 'disabled');
 		});
 
-		$("#btnDel").click(function() {
+		$('#btnDel').click(function() {
 			var num = $('.clonedInput').length;
 			$('#input' + num).remove();
-			$('#btnAdd').attr('disabled', '');
+			//$('#btnAdd').attr('disabled', '');
 			if (num - 1 == 1)
 				$('#btnDel').attr('disabled', 'disabled');
 		});
+
+
 		$('#btnDel').attr('disabled', 'disabled');
 	</script>
-
+	<!---------------------- Zutat Addieren Script ---------------------->
+	<?php 
+	$conn = null;
+	?>
 	<?php
 include('module/footer.php')
 ?>
