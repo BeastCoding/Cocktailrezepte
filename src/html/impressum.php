d30b9af3219bcfdfd6f2ae674e56aee0e473aa03
Marvin2018 <45269715+Marvin2018@users.noreply.github.com>
2019-01-05 14:21:00 +0100

    Weitere CSS Aktualisierung
    CSS Aktualisierung

--- src/html/impressum.php
+++ src/html/impressum.php
@@ -8,59 +8,56 @@ include('module/header.php')
 		<div class="space1"></div>
 		<section id="hauptteil">
 			
-			<section id="hauptteil_schriftarten">
-				<div id="scharfgestochen">
-					<h1>Impressum</h1>
-					<p><span class="imp">
-					Bastian Blawath<br/>E-Mail: bastian.blawath@hhu.de<br/><br/>
-					Burak Kalembasi<br/>E-Mail: burak.kalembasi@hhu.de<br/><br/> 
-					Norbert Ulrich<br/>E-Mail: norbert.ulrich@hhu.de<br/><br/> 
-					
-					Inhaltlich Verantwortlicher gem. § 55 II RStV:<br/> 
-					
-					Norbert Ulrich<br/>E-Mail: norbert.ulrich@hhu.de<br/><br/>	
-					</span></p>
-					<h1>Disclaimer</h1>
-					<p><span class="pos">Die kostenlosen und frei zugänglichen Inhalte dieser Webseite
-						wurden mit größtmöglicher Sorgfalt erstellt. Der Anbieter dieser Webseite
-						übernimmt jedoch keine Gewähr für die Richtigkeit und Aktualität der
-						bereitgestellten kostenlosen und frei zugänglichen journalistischen
-						Ratgeber und Nachrichten.
-					</span></p>
-					<p><span class="pos">
-						Allein durch den Aufruf der kostenlosen und frei zugänglichen Inhalte kommt
-						keinerlei Vertragsverhältnis zwischen dem Nutzer und dem Anbieter zustande,
-						insoweit fehlt es am Rechtsbindungswillen des Anbieters.
-					</span></p>
-					<p><span class="pos">	
-						Die auf dieser Website veröffentlichten Inhalte unterliegen dem deutschen
-						Urheber- und Leistungsschutzrecht. Jede vom deutschen Urheber- und Leistungsschutzrecht
-						nicht zugelassene Verwertung bedarf der vorherigen schriftlichen Zustimmung des Anbieters
-						oder jeweiligen Rechteinhabers. Dies gilt insbesondere für Vervielfältigung, Bearbeitung,
-						Übersetzung, Einspeicherung, Verarbeitung bzw. Wiedergabe von Inhalten in Datenbanken
-						oder anderen elektronischen Medien und Systemen. Inhalte und Rechte Dritter sind dabei
-						als solche gekennzeichnet. Die unerlaubte Vervielfältigung oder Weitergabe einzelner
-						Inhalte oder kompletter Seiten ist nicht gestattet und strafbar. Lediglich die
-						Herstellung von Kopien und Downloads für den persönlichen, privaten und nicht
-						kommerziellen Gebrauch ist erlaubt.
-					</span></p>
-					<p><span class="pos">
-						Die Darstellung dieser Website in fremden Frames ist nur mit schriftlicher Erlaubnis zulässig.
-					</span></p><br/>
-					<h1>Urheber- und Leistungsschutzrecht</h1>
-					<p><span class="pos">
-					Die verwendeten Fotos sind der Webseite www.pexels.com der Pexels GmbH, Franz-Schubert-Str. 20, 34277 Fuldabrück entnommen.
-					</span></p>
-					<p><span class="pos">
-					Ausdrücklich können alle Fotos auf Pexels kostenlos für kommerzielle und nichtkommerzielle Zwecke verwendet werden.
-					Eine Namensnennung ist nicht erforderlich. Eine Erwähnung des Fotografen oder Pexels ist nicht notwendig.
-					</span></p>
-					<p><span class="pos">
-					Die Fotos dürfen bearbeitet werden, eine kreative Veränderung der Fotos ist zum Teil erfolgt. 
-					</span></p>
-				</div> 
-			</section>	
-			
+			<div id="scharfgestochen">
+				<h1>Impressum</h1>
+				<p><span class="imp">
+				Bastian Blawath<br/>E-Mail: bastian.blawath@hhu.de<br/><br/>
+				Burak Kalembasi<br/>E-Mail: burak.kalembasi@hhu.de<br/><br/> 
+				Norbert Ulrich<br/>E-Mail: norbert.ulrich@hhu.de<br/><br/> 
+				
+				Inhaltlich Verantwortlicher gem. § 55 II RStV:<br/> 
+				
+				Norbert Ulrich<br/>E-Mail: norbert.ulrich@hhu.de<br/><br/>	
+				</span></p>
+				<h1>Disclaimer</h1>
+				<p><span class="pos">Die kostenlosen und frei zugänglichen Inhalte dieser Webseite
+					wurden mit größtmöglicher Sorgfalt erstellt. Der Anbieter dieser Webseite
+					übernimmt jedoch keine Gewähr für die Richtigkeit und Aktualität der
+					bereitgestellten kostenlosen und frei zugänglichen journalistischen
+					Ratgeber und Nachrichten.
+				</span></p>
+				<p><span class="pos">
+					Allein durch den Aufruf der kostenlosen und frei zugänglichen Inhalte kommt
+					keinerlei Vertragsverhältnis zwischen dem Nutzer und dem Anbieter zustande,
+					insoweit fehlt es am Rechtsbindungswillen des Anbieters.
+				</span></p>
+				<p><span class="pos">	
+					Die auf dieser Website veröffentlichten Inhalte unterliegen dem deutschen
+					Urheber- und Leistungsschutzrecht. Jede vom deutschen Urheber- und Leistungsschutzrecht
+					nicht zugelassene Verwertung bedarf der vorherigen schriftlichen Zustimmung des Anbieters
+					oder jeweiligen Rechteinhabers. Dies gilt insbesondere für Vervielfältigung, Bearbeitung,
+					Übersetzung, Einspeicherung, Verarbeitung bzw. Wiedergabe von Inhalten in Datenbanken
+					oder anderen elektronischen Medien und Systemen. Inhalte und Rechte Dritter sind dabei
+					als solche gekennzeichnet. Die unerlaubte Vervielfältigung oder Weitergabe einzelner
+					Inhalte oder kompletter Seiten ist nicht gestattet und strafbar. Lediglich die
+					Herstellung von Kopien und Downloads für den persönlichen, privaten und nicht
+					kommerziellen Gebrauch ist erlaubt.
+				</span></p>
+				<p><span class="pos">
+					Die Darstellung dieser Website in fremden Frames ist nur mit schriftlicher Erlaubnis zulässig.
+				</span></p><br/>
+				<h1>Urheber- und Leistungsschutzrecht</h1>
+				<p><span class="pos">
+				Die verwendeten Fotos sind der Webseite www.pexels.com der Pexels GmbH, Franz-Schubert-Str. 20, 34277 Fuldabrück entnommen.
+				</span></p>
+				<p><span class="pos">
+				Ausdrücklich können alle Fotos auf Pexels kostenlos für kommerzielle und nichtkommerzielle Zwecke verwendet werden.
+				Eine Namensnennung ist nicht erforderlich. Eine Erwähnung des Fotografen oder Pexels ist nicht notwendig.
+				</span></p>
+				<p><span class="pos">
+				Die Fotos dürfen bearbeitet werden, eine kreative Veränderung der Fotos ist zum Teil erfolgt. 
+				</span></p>
+			</div> 
 		</section>
 <?php
 include('module/footer.php')
--- src/html/rel_modell.php
+++ src/html/rel_modell.php
@@ -7,8 +7,11 @@ include('module/header.php')
 		?>
 		<div class="space1"></div>
 		<section id="hauptteil">
-
-				<img class="rel_modell" src="../img/Rel Modell final.jpg" alt="Relation_Modell">			
+			<img class="ueberschrift"src="../img/Überschrift.jpg" alt="Ueberschrift">
+			<img class="relationen"src="../img/Relationen.jpg" alt="Relationen">
+			<img class="beziehungen" src="../img/Beziehungen.jpg" alt="Beziehungen">	
+			<img class="gruppe" src="../img/Gruppe 5.jpg" alt="Gruppe 5">
+		<!--		<img class="rel_modell" src="../img/Rel Modell final.jpg" alt="Relation_Modell">	-->		
 
 
 		</section>
